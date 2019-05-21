a9f73568d0ed496dbed582985694f6a3e7c675e5
Vipul Srivastava <vipulsrivastava@igotchamedia.com>
2019-05-21 06:10:43 -0400

    app component html changed, added template html for container

--- client/src/app/container/temperature-log-container.ts
+++ client/src/app/container/temperature-log-container.ts
@@ -1,5 +1,10 @@
+import { Component} from '@angular/core';
 import {TempLog} from '../components/temp-log';
 
+@Component({
+    selector: 'temperatureLog',
+    templateUrl: './temperature-log-container.component.html'
+  })
 export class TemperatureLogContainer {
     temperatureLogs: TempLog[];
 }
