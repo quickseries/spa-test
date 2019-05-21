7e420513886762811b132f0fd89cade419cc49fa
Vipul Srivastava <vipulsrivastava@igotchamedia.com>
2019-05-21 05:11:22 -0400

    initial commit, connected to mongodb mLab instance, dependencies installed

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://quickseries:quickseries1@ds157946.mlab.com:57946/quick-series'
+};
+   
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 5000;
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(morgan('dev'));
+app.use(cors())
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to db
+mongoose
+	.connect(db, {useNewUrlParser: true})
+	.then( () => console.log('Mongo DB Connected!'))
+    .catch(err  => console.log(err));
+    
+
+// @route GET /
+// @desc  backend test route 
+// @access Public
+app.get('/', (req,res)=>{
+    res.send('quick series backend working')  
+})
+.listen(PORT, () => console.log('Listening at port :' + PORT));
