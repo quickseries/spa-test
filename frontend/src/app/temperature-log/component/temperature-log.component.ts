7843f393ea44d7e24a433c66ebacf4dbf3d247fc
kaushiksamanta <kaushiksamanta23@gmail.com>
2019-05-28 09:42:25 -0400

    temperature log service path changed

--- frontend/src/app/temperature-log/component/temperature-log.component.ts
+++ frontend/src/app/temperature-log/component/temperature-log.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { TemperatureLogService } from '../service/temperature-log.service';
+import { TemperatureLogService } from '../services/temperature-log.service';
 
 @Component({
   selector: 'app-temperature-log',
